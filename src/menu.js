9dafa3ff5fa02326ef8b6c32c53b255b33729634
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-07-31 20:56:57 -0300

    add headerDiv and h1Text to menu

--- src/menu.js
+++ src/menu.js
@@ -4,6 +4,14 @@ const nav = document.querySelector("nav");
 
 function generateMenu() {
   contentDiv.innerHTML = "";
+
+  const headerDiv = document.createElement("div");
+  headerDiv.classList.add("menu-header");
+  contentDiv.appendChild(headerDiv);
+
+  const h1Text = document.createElement("h1");
+  h1Text.textContent = "The Nordic Menu";
+  headerDiv.appendChild(h1Text);
 }
 
 export {generateMenu}
