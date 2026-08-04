a2ac90f72ee4e9ef7afe868ddde5c984edc17e28
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-08-04 18:02:37 -0300

    add aboutContainer

--- src/about.js
+++ src/about.js
@@ -4,4 +4,9 @@ function generateAbout() {
   contentDiv.style.display = "block";
   contentDiv.style.alignItems = "initial";
   contentDiv.style.justifyContent = "initial";
+
+  const aboutContainer = document.createElement("div");
+  aboutContainer.classList.add("about-container");
+  contentDiv.appendChild(aboutContainer);
+
 }
