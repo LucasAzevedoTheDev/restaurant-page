b03bef18cfda8a639dd91724b80eac12f8147c28
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-08-04 18:06:37 -0300

    add aboutCard div

--- src/about.js
+++ src/about.js
@@ -8,6 +8,10 @@ function generateAbout() {
   const aboutContainer = document.createElement("div");
   aboutContainer.classList.add("about-container");
   contentDiv.appendChild(aboutContainer);
+
+  const aboutCard = document.createElement("div");
+  aboutCard.classList.add("about-card");
+  aboutContainer.appendChild(aboutCard);
 }
 
 export{generateAbout};
