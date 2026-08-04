6f14f9f485af81252cccbf7a29a53fec40147f4d
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-08-04 18:01:24 -0300

    add about.js boilerplate

---
+++ src/about.js
@@ -0,0 +1,7 @@
+const contentDiv = document.querySelector("#content");
+
+function generateAbout() {
+  contentDiv.style.display = "block";
+  contentDiv.style.alignItems = "initial";
+  contentDiv.style.justifyContent = "initial";
+}
