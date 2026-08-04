11f9023d338cce5b381941984e70455adacb9070
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-08-04 18:45:58 -0300

    add first contact card

--- src/contact.js
+++ src/contact.js
@@ -25,6 +25,25 @@ function generateContact() {
   contactContainer.classList.add("contact-container");
   contentDiv.appendChild(contactContainer);
 
+  const contactCard1 = document.createElement("div");
+  
+  const contactCard1Head = document.createElement("h3");
+  contactCard1Head.textContent = "Location & Hours";
+
+  const contactCard1P1 = document.createElement("p");
+  contactCard1P1.textContent = "442 Emberwood Trail, Northern Ridge";
+
+  const contactCard1P2 = document.createElement("p");
+  contactCard1P2.textContent = "Tuesday – Sunday: 5:00 PM – 11:00 PM";
+
+  const contactCard1P3 = document.createElement("p");
+  contactCard1P3.textContent = "(Mondays closed for wood gathering)";
+
+  contactCard1.appendChild(contactCard1Head);
+  contactCard1.appendChild(contactCard1P1);
+  contactCard1.appendChild(contactCard1P2);
+  contactCard1.appendChild(contactCard1P3);
+  contactContainer.appendChild(contactCard1);
   
 }
 
